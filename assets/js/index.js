211b7006e838f791f9aaf07c3b380ed82f81adb7
Gabriel Meza <108927775+Gabotox@users.noreply.github.com>
2023-03-29 21:35:05 -0500

    Update index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,33 +1,3 @@
-let abrir = document.querySelector(".responsive");
-let cerrar = document.querySelector(".cerrar");
-let menu = document.querySelector(".navContacto");
-let itemsMenu = document.querySelectorAll(".item");
-
-
-abrir.addEventListener("click", mostrar);
-cerrar.addEventListener("click", ocultar);
-
-
-// Función que muestra el menú responsive
-function mostrar() {
-    menu.style.left = '0px';
-}
-// Función que oculta el menú responsive
-function ocultar() {
-    menu.style.left = '-200%';
-}
-// Un foreach para recorrer todos los elementos del menú, para cuando se
-// haga click en alguno se oculte
-itemsMenu.forEach(element => {
-    element.addEventListener("click", ocultar);
-});
-
-// Creando el slider de SwiperJS
-var swiper = new Swiper(".mySwiper", {
-    loop: true,
-    fade: "true",
-    autoplay: {
-        delay: 5000, // Tiempo de espera entre cada slide
-        disableOnInteraction: false, // No detener la reproducción cuando el usuario interactúa con el slide
-    },
-});
+let abrir=document.querySelector(".responsive");let cerrar=document.querySelector(".cerrar");let menu=document.querySelector(".navContacto");let itemsMenu=document.querySelectorAll(".item");abrir.addEventListener("click",mostrar);cerrar.addEventListener("click",ocultar);function mostrar(){menu.style.left='0px'}
+function ocultar(){menu.style.left='-200%'}
+itemsMenu.forEach(element=>{element.addEventListener("click",ocultar)})
